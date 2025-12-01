b623332e52c945477fd560f9c05d48d41f9ccfd2
Maxwell Azubuike <127757943+maxwell006@users.noreply.github.com>
2025-12-01 01:14:43 +0100

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,15 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
